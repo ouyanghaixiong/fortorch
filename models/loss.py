fix: the error of calculating of gradient norm

models/loss.py
@@ -210,13 +210,18 @@ class GHMCrossEntropy(torch.nn.Module):
         if y_pred.dim() != y_true.dim():
             raise ValueError(f"the dim of y_pred {y_pred.dim()} != the dim of y_true {y_true.dim()}")
         if label_weight is None:
-            label_weight = torch.ones_like(y_pred)
-        y_true, label_weight = y_true.float(), label_weight.float()
-        weights = torch.zeros_like(label_weight)
+            label_weight = torch.ones(size=(y_pred.size()[0], 1))
+        label_weight = label_weight.float()
+        weights = torch.zeros(size=(y_pred.size()[0], 1))
 
         # gradient length, shape [batch_size, 1]
         with torch.no_grad():
-            g = torch.sum(torch.abs(y_true * (1 - y_pred.sigmoid())), dim=1, keepdim=True)
+            g = torch.sum(
+                torch.abs(
+                    torch.gather(1 - torch.softmax(y_pred, dim=1), dim=1, index=y_true)
+                ),
+                dim=1, keepdim=True
+            )
 
         valid = label_weight > 0
         tot = max(valid.float().sum().item(), 1.0)
@@ -235,6 +240,9 @@ class GHMCrossEntropy(torch.nn.Module):
         if n > 0:
             weights = weights / n
 
-        loss = F.binary_cross_entropy_with_logits(y_pred, y_true, weight=weights, reduction=self.reduction)
+        loss_ce = F.cross_entropy(y_pred, y_true.reshape(-1), reduce=False)
+        print(loss_ce)
+        loss = weights * loss_ce.reshape(-1, 1)
+        print(loss)
 
-        return loss
+        return loss.sum() if self.reduction == 'sum' else loss.mean()

test/test_loss.py
@@ -10,7 +10,7 @@ from unittest import TestCase
 import torch
 
 from consts import DTYPE, DEVICE
-from models.loss import FocalCrossEntropy, FocalBinaryCrossEntropy, GHMBinaryCrossEntropy
+from models.loss import FocalCrossEntropy, FocalBinaryCrossEntropy, GHMBinaryCrossEntropy, GHMCrossEntropy
 
 
 class TestFocalCrossEntropy(TestCase):
@@ -67,3 +67,28 @@ class TestGHMBinaryCrossEntropy(TestCase):
         loss1 = criterion(logit, y_true)
         self.assertTrue(loss0.item() > 0)
         self.assertTrue(loss1.item() < loss0.item())
+
+
+class TestGHMCrossEntropy(TestCase):
+    def test_forward(self):
+        criterion = GHMCrossEntropy(bins=10, momentum=0, reduction="mean")
+
+        logit = torch.tensor([[1, 0, 0], [2, 0, 0], [3, 0, 0], [4, 0, 0], [5, 0, 0]], dtype=torch.float32,
+                             device=DEVICE)
+        y_true = torch.tensor([[1], [1], [1], [1], [1]], dtype=torch.int64, device=DEVICE)
+        loss0 = criterion(logit, y_true)
+        print(loss0)
+        self.assertTrue(loss0.item() > 0)
+
+        logit = torch.tensor([[1, 5, 10], [2, 7, 8], [3, 1, 9], [4, 5, 99], [5, 66, 999]], dtype=DTYPE, device=DEVICE)
+        y_true = torch.tensor([[2], [2], [2], [2], [2]], dtype=torch.int64, device=DEVICE)
+        loss1 = criterion(logit, y_true)
+        self.assertTrue(loss1.item() > 0)
+        self.assertTrue(loss1.item() < loss0.item())
+        print(loss1)
+
+        logit = torch.tensor([[0, 0, 10], [0, 0, 8], [0, 0, 9], [0, 0, 99], [0, 0, 999]], dtype=DTYPE, device=DEVICE)
+        y_true = torch.tensor([[2], [2], [2], [2], [2]], dtype=torch.int64, device=DEVICE)
+        loss2 = criterion(logit, y_true)
+        self.assertTrue(loss2.item() < loss1.item())
+        print(loss2)